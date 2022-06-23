49b91aef1d32b662311fc08953732088ca0ba023
bello <bellosoliu12@gmail.com>
2022-06-24 00:46:55 +0100

    first commit

---
+++ calculator.js
@@ -0,0 +1,24 @@
+function addition(a, b) {
+  result = a + b;
+  console.log(result);
+}
+
+function substraction(a, b) {
+  result = a - b;
+  console.log(result);
+}
+
+function multiplication(a, b) {
+  result = a * b;
+  console.log(result);
+}
+
+function division(a, b) {
+  result = a / b;
+  console.log(result);
+}
+
+addition(8, 2);
+substraction(20, 2);
+multiplication(18, 4);
+division(8, 2);
